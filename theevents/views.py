0bb3354727144d2cc33c448cb48b7c287682481e
Abdulrahman Alazwari <aazwari@gmail.com>
2022-08-06 02:24:14 +0300

    update add attendnce view

--- theevents/views.py
+++ theevents/views.py
@@ -3,7 +3,7 @@ from .models import Event, Attendence
 from rest_framework.response import Response
 from rest_framework.request import Request
 import pandas as pd
-from django.core.validators import validate_email
+
 
 from rest_framework.decorators import api_view, authentication_classes, permission_classes
 from rest_framework.status import HTTP_201_CREATED, HTTP_401_UNAUTHORIZED, HTTP_403_FORBIDDEN
@@ -37,27 +37,25 @@ def add_attendences(request):
         file = request.FILES['file']
         df = pd.read_excel(file)
         for index, row in df.iterrows():
-            if validate_email(row['email']) is not None:
-                attendence = Attendence(event=event, name=row['name'], email=row['email'], mobile=row['mobile'])
-                attendence.save()
-            else:
-                pass
+            name = row['name']
+            email = row['email']
+            mobile = row['mobile']
+            attendence = Attendence(event=event, name=name, email=email, mobile=mobile)
+            attendence.save()
         return Response({"message": "Attendences added successfully."})
     
 
 # get envent details
-@api_view(['GET'])
-# @authentication_classes([JWTAuthentication])
-# @permission_classes([IsAuthenticated])
+@api_view(['POST'])
+@authentication_classes([JWTAuthentication])
+@permission_classes([IsAuthenticated])
 def get_event_details(request, pk):
     event = get_object_or_404(Event, pk=pk)
-    # events = [
-    #     {'id': event.id, 'name': event.name, 'description': event.description, 'location': event.location, 'start_date': event.start_date, 'end_date': event.end_date}
-    # ]
+    events = [
+        {'id': event.id, 'name': event.name, 'description': event.description, 'location': event.location, 'start_date': event.start_date, 'end_date': event.end_date}
+    ]
 
-    contenxt = {
-        'events': event
-    }
-
-    return render(request, 'theevents/event_details.html', contenxt)
+    return Response({
+        'events': events
+    })
 
