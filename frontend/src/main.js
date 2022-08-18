38203b1f16d22ca1d50971bd0a9da513d343c99f
Sufyan Farea Mohammed <sfmu1@sufyan.sufyan>
2022-08-19 01:36:24 +0300

    add frontend

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,11 +1,11 @@
 import { createApp } from 'vue'
+import './app.css'
 import App from './App.vue'
 import router from './router'
+import store from './store'
+import axios from 'axios'
 
-import './assets/main.css'
+createApp(App).use(store).use(router, axios).mount('#app')
 
-const app = createApp(App)
-
-app.use(router)
-
-app.mount('#app')
+axios.defaults.baseURL = import.meta.env.VITE_API_URL
+axios.defaults.headers.post['Authorization'] = `Bearer ${store.state.auth.token}`;
